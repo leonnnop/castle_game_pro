6df78df854a8d811307a20fae44f2e47a04246ce
leonnnop <277284652@qq.com>
2017-08-17 02:38:24 +0800

    更新地图组件

--- castle/HandlerHelp.java
+++ castle/HandlerHelp.java
@@ -7,7 +7,10 @@ public class HandlerHelp extends Handler {
 	
 	@Override
 	public void doCmd(String word) {
-        System.out.print("迷路了吗？你可以做的命令有：go bye help");
+        System.out.print("迷路了吗？你可以做的命令有：go bye help map");
+        System.out.println("map用于查看已经走过的屋子的信息。");
+        System.out.println("role用于查看当前角色的血量。");
+        System.out.println("go xxx用于前往你想去的地方。");
         System.out.println("\t如：go east");
 	}
 
