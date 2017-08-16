43c93286432b1fc47cdfb5571d32b623c9a3bb66
leonnnop <277284652@qq.com>
2017-08-16 18:59:36 +0800

    基础城堡游戏内容

---
+++ castle/HandlerHelp.java
@@ -0,0 +1,15 @@
+package castle;
+
+public class HandlerHelp extends Handler {
+
+	public HandlerHelp(Game game){
+		super(game);
+	}
+	
+	@Override
+	public void doCmd(String word) {
+        System.out.print("迷路了吗？你可以做的命令有：go bye help");
+        System.out.println("\t如：go east");
+	}
+
+}
